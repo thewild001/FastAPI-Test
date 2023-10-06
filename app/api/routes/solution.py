974596894b2c04906c78a70f51d929d175390de3
M1GU3L <mmramirez@uci.cu>
2023-10-05 22:02:35 -0400

    [CHG] Refactoring endpont

--- app/api/routes/solution.py
+++ app/api/routes/solution.py
@@ -12,6 +12,6 @@ router = APIRouter()
 async def process_orders(request: Request, orderRequest: orderModel.OrderRequest = Body(...)):
     total_price = service_order.process_orders(orderRequest.orders, orderRequest.criterion)
     print(f"Ingresos totales para órdenes '{orderRequest.criterion}' es: ${total_price}")
-    return {"total_price": total_price}
+    return  total_price
 
 
