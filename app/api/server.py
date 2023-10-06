e171194ca357ebe3ad9cba0437fb186562261949
M1GU3L <mmramirez@uci.cu>
2023-10-05 22:42:02 -0400

    [CHG] Refactoring

--- app/api/server.py
+++ app/api/server.py
@@ -1,13 +1,15 @@
+from datetime import timedelta
+
 from fastapi import FastAPI
+from fastapi_redis_cache import cache
+
 from app.core.configuration import config, swagger
 
 # Initialize APP
 app = config.get_application()
 
-
 # SWAGGER UI CONFIG
 app = swagger.init_swagger_ui()
-
 
 # APP ROUTES
 from app.api.routes import router as api_router
@@ -15,6 +17,7 @@ app.include_router(api_router)
 
 
 @app.get("/")
+@cache(expire=timedelta(minutes=5))
 def get_data():
     return {"message": "Fonoma - Backend Developer Test!"}
 
