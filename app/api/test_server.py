05666562ddb78ebfa4723403207ebdce51b605d5
M1GU3L <mmramirez@uci.cu>
2023-10-05 21:35:31 -0400

    [ADD] Unit test

--- app/api/test_server.py
+++ app/api/test_server.py
@@ -4,6 +4,10 @@ from .server import app
 client = TestClient(app)
 
 def test_proccess_order():
+    """
+    Test the '/solution' endpoint.
+    """
+
     request_data = {
         "orders": [
             {"id": 1, "item": "Laptop", "quantity": 1, "price": 999.99, "status": "completed"},
@@ -14,11 +18,66 @@ def test_proccess_order():
         "criterion": "completed"
     }
 
-    response = client.post("/solution",
-                        headers={"X-Token": "coneofsilence"},
-                        json=request_data
-                        )
+    response = client.post("/solution", json=request_data)
     assert response.status_code == 200
+
+def test_process_order_completed():
+    """
+    Test the endpoint '/solution' with the criterion 'completed' to calculate the total revenue.
+    """
+
+    request_data = {
+        "orders": [
+            {"id": 1, "item": "Laptop", "quantity": 1, "price": 999.99, "status": "completed"},
+            {"id": 2, "item": "Smartphone", "quantity": 2, "price": 499.95, "status": "completed"},
+        ],
+        "criterion": "completed"
+    }
+
+    
+    response = client.post("/solution", json=request_data)
+    
+    assert response.status_code == 200
+    assert response.json() == {"total_revenue": 1499.94}
+
+def test_process_order_pending():
+    """
+    Test the endpoint '/solution' with the criterion 'pending' to calculate the total revenue.
+    """
+
+    request_data = {
+        "orders": [
+            {"id": 1, "item": "Laptop", "quantity": 1, "price": 999.99, "status": "completed"},
+            {"id": 2, "item": "Smartphone", "quantity": 2, "price": 499.95, "status": "pending"},
+        ],
+        "criterion": "pending"
+    }
+
+    response = client.post("/solution", json=request_data)
+
+    assert response.status_code == 200
+    assert response.json() == {"total_revenue": 1999.89}
+
+def test_process_order_all():
+    """
+    Try the endpoint '/solution' with the criterion 'all' to calculate the total revenue of all orders.
+    """
+
+    request_data = {
+        "orders": [
+            {"id": 1, "item": "Laptop", "quantity": 1, "price": 999.99, "status": "completed"},
+            {"id": 2, "item": "Smartphone", "quantity": 2, "price": 499.95, "status": "pending"},
+        ],
+        "criterion": "all"
+    }
+
+
+    response = client.post("/solution", json=request_data)
+
+    assert response.status_code == 200
+    assert response.json() == {"total_revenue": 1999.89}
+
+
     
 
 
